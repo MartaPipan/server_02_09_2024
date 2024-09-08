dee03f01bd9a896bd71279bc62c6773798d8bfe2
Marta Prystatska <tradlisboa@gmail.com>
2024-09-08 15:13:00 +0100

    add method POST (add client; user),   how work debager?

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('node:http');
 const fs = require('fs');
 const PORT = 3000;
+const users = [];
 
 const server = http.createServer((req, res) => {
     const { method, url } = req;
@@ -32,6 +33,23 @@ const server = http.createServer((req, res) => {
             });
                return;
            }
+    }
+    if (method === 'POST') {
+        if (url === '/users') {
+            let jsonStr = '';
+            req.on('data', (chunk) => {
+                jsonStr += chunk;
+            })
+            req.on('end', () => {
+                const user = JSON.parse(jsonStr);
+                delete user.password;
+                user.id = Date.now();
+                users.push(user);
+                console.log(users);
+                res.end(JSON.stringify(user));
+            })
+            return;
+        }
     }
     fs.readFile('./views/404.html', { encoding: 'utf8' }, (err, data) => {
         if (err) {
