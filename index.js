e17ec0becfb050dc21dd881ce2852ebdc151dd04
Marta Prystatska <tradlisboa@gmail.com>
2024-09-08 11:59:37 +0100

    RESPONSE (read index.html with callback)

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const http = require('node:http');
+const fs = require('fs');
 const PORT = 3000;
 const server = http.createServer((req, res) => {
-    console.log('req', req);
-    console.log('res', res);
-    res.end('hello from server!')
+    fs.readFile('./views/index.html', { encoding: 'utf8' }, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.end(data);
+    });
 });
+    //read index.html with callback
+   
 server.listen(PORT, () => {
     console.log('server start at port ' + PORT);
 });
