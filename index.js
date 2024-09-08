6b243e807fff9419a83d58496f79dfc0099679d9
Marta Prystatska <tradlisboa@gmail.com>
2024-09-08 11:22:22 +0100

    create SERVER

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi!');
+const http = require('node:http');
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    console.log('req', req);
+    console.log('res', res);
+    res.end('hello from server!')
+});
+server.listen(PORT, () => {
+    console.log('server start at port ' + PORT);
+});
